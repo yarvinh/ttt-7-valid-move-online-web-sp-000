b5183f09c39c5012b3b01c1c6f8d31cd4c01e3d5
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-10 00:35:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def valid_move(board,index)
-  if board[index] = true
+  if board[index] == true
     true
   end
 end
