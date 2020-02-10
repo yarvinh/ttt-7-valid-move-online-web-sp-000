344c3a097af77980a072de4accc9fb6808f51fa8
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-10 00:42:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,18 @@
 # code your #valid_move? method here
 
-def valid_move(board,index)
+def valid_move?(board,index)
   if board[index]
     true
   end
 end
+
+# describe './lib/valid_move.rb' do
+#   it 'returns true for a valid position on an empty board' do
+#     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+#     index = 0
+#     expect(valid_move?(board, index)).to be_truthy
+#   end
+
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
