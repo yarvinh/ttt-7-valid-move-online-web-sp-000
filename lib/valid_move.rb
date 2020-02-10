e6f4b4c48b21e93c56bc373bf91df5248ac391ca
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-10 00:46:39 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,14 @@
 # code your #valid_move? method here
 
+def valid_move?(board,index)
+  if board[index] == " "
+    true
+  elsif board[index] == "X"
+  end
+end
+
+
+
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
